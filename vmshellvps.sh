0f053a0d52c3dd94fd8bdfdd3d1081603b79bd9c
smallram <75463378+FoxBary@users.noreply.github.com>
2021-12-13 20:03:06 +0800

    Update vmshellvps.sh

--- vmshellvps.sh
+++ vmshellvps.sh
@@ -4,8 +4,7 @@ if [ -f /etc/redhat-release ]; then
     release="centos"
 elif cat /etc/issue | grep -Eqi "debian"; then
     release="debian"
-elif cat /etc/issue | grep -Eqi "ubuntu"; then
-    release="ubuntu"
+
 elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
     release="centos"
 elif cat /proc/version | grep -Eqi "debian"; then
